c2c46ca74a24aad2c04d6bcbf610980acf84cc38
dxeware <dxeware@gmail.com>
2015-05-13 20:51:47 -0700

    Added color button and jokes

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var express = require("express");
 var random = require("./random-color");
 var jokes = require("./jokes");
 var app = express();
+var path = require("path");
 var port = process.env.PORT || 3000;
+
+app.use( express.static(path.join(__dirname, "app")));
 
 app.listen(port, function() {
   console.log('app server started on port', port);
